11c059ebdcf1ed3acc0a9243f157deaa081bf799
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2009-06-06 20:06:28 +0300

    Extent storage support.

--- org/jpc/support/ImageMaker.java
+++ org/jpc/support/ImageMaker.java
@@ -55,7 +55,7 @@ public class ImageMaker
 
     static class NormalDiskImage extends DiskImageType
     {
-        public int saveSize(int code, int[] sectormap, int totalSectors, int sectorsUsed)
+        public int saveSize(int code, int[] sectormap, int totalSectors, int sectorsUsed) throws Exception
         {
             return 512 * sectorsUsed;
         }
@@ -84,7 +84,7 @@ public class ImageMaker
 
     static class SectorMapDiskImage extends DiskImageType
     {
-        public int saveSize(int code, int[] sectormap, int totalSectors, int sectorsUsed)
+        public int saveSize(int code, int[] sectormap, int totalSectors, int sectorsUsed) throws Exception
         {
             int sectorMapSize = (sectorsUsed + 7) / 8;
             int sectorsInUse = 0;
@@ -132,10 +132,139 @@ public class ImageMaker
         }
     }
 
+    static class ExtentDiskImage extends DiskImageType
+    {
+        public int saveSize(int code, int[] sectormap, int totalSectors, int sectorsUsed) throws Exception
+        {
+            if((code & 1) != (sectormap[0] & 1))
+                throw new Exception("Sector 0 wrong type for method.");
+
+            int extentsSize = 0;
+            int extentSize = 0;
+            int sectorsInUse = 0;
+            int currentType = -1;
+
+            extentSize = 4;
+            if(sectorsUsed <= 16777216)
+                extentSize = 3;
+            if(sectorsUsed <= 65536)
+                extentSize = 2;
+            if(sectorsUsed <= 256)
+                extentSize = 1;
+
+            for(int i = 0; i < sectorsUsed; i++) {
+                boolean present = ((sectormap[i / 31] & (1 << (i % 31))) != 0);
+                if(present)
+                    sectorsInUse++;
+                if(present && currentType != 1) {
+                    extentsSize += extentSize;
+                    currentType = 1;
+                }
+                if(!present && currentType != 0) {
+                    extentsSize += extentSize;
+                    currentType = 0;
+                }
+            }
+
+            return 512 * sectorsInUse + extentsSize;
+        }
+
+        public void save(int code, int[] sectormap, RandomAccessFile rawImage, int totalSectors, int sectorsUsed, 
+            RandomAccessFile output) throws IOException
+        {
+            if((code & 1) != (sectormap[0] & 1))
+                throw new IOException("Sector 0 wrong type for method.");
+
+            int extentSize = 0;
+            extentSize = 4;
+            if(sectorsUsed <= 16777216)
+                extentSize = 3;
+            if(sectorsUsed <= 65536)
+                extentSize = 2;
+            if(sectorsUsed <= 256)
+                extentSize = 1;
+            byte[] extentBuf = new byte[extentSize];
+            int groupExpiry = 0;
+
+            byte[] sector = new byte[512];
+            rawImage.seek(0);
+            for(int i = 0; i < sectorsUsed; i++) {
+                if(rawImage.read(sector) != 512) {
+                    throw new IOException("Can't read disk image sector " + i + ".");
+                }
+                if(i == groupExpiry) {
+                    //Group Expired. Find the new expiry point.
+                    int oldExpiry = i;
+                    boolean firstPresent = ((sectormap[i / 31] & (1 << (i % 31))) != 0);
+                    groupExpiry = sectorsUsed;
+                    for(int j = i + 1; j < sectorsUsed; j++) {
+                        boolean secondPresent = ((sectormap[j / 31] & (1 << (j % 31))) != 0);
+                        if(secondPresent != firstPresent) {
+                            groupExpiry = j;
+                            break;
+                        }
+                    }
+                    int extent = groupExpiry - oldExpiry - 1;
+                    extentBuf[0] = (byte)((extent) & 0xFF);
+                    if(extentSize > 1)
+                        extentBuf[1] = (byte)((extent >>> 8) & 0xFF);
+                    if(extentSize > 2)
+                        extentBuf[2] = (byte)((extent >>> 16) & 0xFF);
+                    if(extentSize > 3)
+                        extentBuf[3] = (byte)((extent >>> 24) & 0xFF);
+                    output.write(extentBuf);
+                }
+                if((sectormap[i / 31] & (1 << (i % 31))) != 0)
+                    output.write(sector);
+            }
+        }
+
+        public int[] loadSectorMap(RandomAccessFile image, int type, int sectorsUsed, int offset) throws IOException
+        {
+            int[] map = new int[sectorsUsed];
+            boolean present = !((type & 1) != 0);
+            int extentSize = 0;
+            extentSize = 4;
+            if(sectorsUsed <= 16777216)
+                extentSize = 3;
+            if(sectorsUsed <= 65536)
+                extentSize = 2;
+            if(sectorsUsed <= 256)
+                extentSize = 1;
+            byte[] extentBuf = new byte[extentSize];
+            int flipAt = 0;
+
+            for(int i = 0; i < sectorsUsed; i++) {
+                if(i == flipAt) {
+                   int oldFlip = flipAt;
+                   image.seek(offset);
+                   if(image.read(extentBuf) != extentBuf.length) {
+                       throw new IOException("Can't read disk image extent.");
+                   }
+                   flipAt = i + 1 + ((int)extentBuf[0] & 0xFF);
+                   if(extentSize > 1)
+                       flipAt += (((int)extentBuf[1] & 0xFF) << 8);
+                   if(extentSize > 2)
+                       flipAt += (((int)extentBuf[2] & 0xFF) << 16);
+                   if(extentSize > 3)
+                       flipAt += (((int)extentBuf[3] & 0xFF) << 24);
+                   offset += extentSize;
+                   present = !present;
+                }
+                if(present) {
+                     map[i] = offset;
+                     offset += 512;
+                }
+            }
+            return map;
+        }
+    }
+
     static DiskImageType[] savers;
     
     static {
-        savers = new DiskImageType[] {new NormalDiskImage(), new SectorMapDiskImage()};
+        savers = new DiskImageType[] {new NormalDiskImage(), new SectorMapDiskImage(), new ExtentDiskImage(), 
+            new ExtentDiskImage()};
     }
 
     static class ParsedImage
