35493494cb3100fae5ad8d722d15afbdf82e6c9f
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2009-08-15 13:27:08 +0300

    Enhance PCStartStopTest to also send some key events

--- org/jpc/plugins/PCStartStopTest.java
+++ org/jpc/plugins/PCStartStopTest.java
@@ -36,11 +36,13 @@ import javax.swing.*;
 
 import org.jpc.support.Plugins;
 import org.jpc.emulator.PC;
+import org.jpc.emulator.peripheral.Keyboard;
 
 public class PCStartStopTest extends JFrame implements org.jpc.Plugin
 {
     private static final long serialVersionUID = 8;
     private Plugins vPluginManager;
+    private Keyboard keyboard;
 
     public void systemShutdown()
     {
@@ -49,7 +51,11 @@ public class PCStartStopTest extends JFrame implements org.jpc.Plugin
 
     public void reconnect(PC pc)
     {
-        //Not interested.
+        if(pc != null) {
+            keyboard = (Keyboard)pc.getComponent(Keyboard.class);
+        } else {
+            keyboard = null;
+        }
     }
 
     public void pcStarting()
@@ -84,20 +90,124 @@ public class PCStartStopTest extends JFrame implements org.jpc.Plugin
         this.vPluginManager = manager;
 
         JMenuBar bar = new JMenuBar();
+        JMenuItem tmp;
 
         JMenu file = new JMenu("Action");
-        file.add("Start").addActionListener(new ActionListener() {
+        (tmp = file.add("Start")).addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e)
                 {
                     vPluginManager.startPC();
                 }
             });
-        file.add("Stop").addActionListener(new ActionListener() {
+        tmp.setAccelerator(KeyStroke.getKeyStroke("F5"));
+
+        (tmp = file.add("Stop")).addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e)
                 {
                     vPluginManager.stopPC();
                 }
             });
+        tmp.setAccelerator(KeyStroke.getKeyStroke("F6"));
+
+        (tmp = file.add("Send <Left> Edge")).addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e)
+                {
+                    try {
+                        keyboard.sendEdge(128 + 75);
+                    } catch(Exception f) { f.printStackTrace(); }
+                }
+            });
+        tmp.setAccelerator(KeyStroke.getKeyStroke("J"));
+
+        (tmp = file.add("Send <Down> Edge")).addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e)
+                {
+                    try {
+                        keyboard.sendEdge(128 + 80);
+                    } catch(Exception f) { f.printStackTrace(); }
+                }
+            });
+        tmp.setAccelerator(KeyStroke.getKeyStroke("K"));
+
+        (tmp = file.add("Send <Right> Edge")).addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e)
+                {
+                    try {
+                        keyboard.sendEdge(128 + 77);
+                    } catch(Exception f) { f.printStackTrace(); }
+                }
+            });
+        tmp.setAccelerator(KeyStroke.getKeyStroke("L"));
+
+        (tmp = file.add("Send <Up> Edge")).addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e)
+                {
+                    try {
+                        keyboard.sendEdge(128 + 72);
+                    } catch(Exception f) { f.printStackTrace(); }
+                }
+            });
+        tmp.setAccelerator(KeyStroke.getKeyStroke("I"));
+
+        (tmp = file.add("Send <Enter> Edge")).addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e)
+                {
+                    try {
+                        keyboard.sendEdge(28);
+                    } catch(Exception f) { f.printStackTrace(); }
+                }
+            });
+        tmp.setAccelerator(KeyStroke.getKeyStroke("M"));
+
+        (tmp = file.add("Send <Escape> Edge")).addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e)
+                {
+                    try {
+                        keyboard.sendEdge(1);
+                    } catch(Exception f) { f.printStackTrace(); }
+                }
+            });
+        tmp.setAccelerator(KeyStroke.getKeyStroke("N"));
+
+        (tmp = file.add("Send <RShift> Edge")).addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e)
+                {
+                    try {
+                        keyboard.sendEdge(54);
+                    } catch(Exception f) { f.printStackTrace(); }
+                }
+            });
+        tmp.setAccelerator(KeyStroke.getKeyStroke("B"));
+
+        (tmp = file.add("Send <RCTRL> Edge")).addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e)
+                {
+                    try {
+                        keyboard.sendEdge(128 + 29);
+                    } catch(Exception f) { f.printStackTrace(); }
+                }
+            });
+        tmp.setAccelerator(KeyStroke.getKeyStroke("V"));
+
+        (tmp = file.add("Send <RALT> Edge")).addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e)
+                {
+                    try {
+                        keyboard.sendEdge(128 + 56);
+                    } catch(Exception f) { f.printStackTrace(); }
+                }
+            });
+        tmp.setAccelerator(KeyStroke.getKeyStroke("C"));
+
+        (tmp = file.add("Send <SPACE> Edge")).addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e)
+                {
+                    try {
+                        keyboard.sendEdge(57);
+                    } catch(Exception f) { f.printStackTrace(); }
+                }
+            });
+        tmp.setAccelerator(KeyStroke.getKeyStroke("X"));
 
         bar.add(file);
         setJMenuBar(bar);
