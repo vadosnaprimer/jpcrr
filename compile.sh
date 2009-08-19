5ed6f5a1c9a625bcc054d87e9f96c029df01ff03
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2009-08-20 00:58:21 +0300

    Update compile script

--- compile.sh
+++ compile.sh
@@ -4,9 +4,5 @@ find -name "*.class" -exec rm {} \;
 javac -Xlint:unchecked JPCApplication.java
 javac -Xlint:unchecked ImageLibrary.java
 javac -Xlint:unchecked ImageMaker.java
-javac -Xlint:unchecked org/jpc/plugins/VirtualKeyboard.java
-javac -Xlint:unchecked org/jpc/plugins/PNGDumper.java
-javac -Xlint:unchecked org/jpc/plugins/PCMonitor.java
-javac -Xlint:unchecked org/jpc/plugins/PCControl.java
-javac -Xlint:unchecked org/jpc/plugins/PCRunner.java
-javac -Xlint:unchecked org/jpc/plugins/PCStartStopTest.java
+javac -Xlint:unchecked org/jpc/plugins/*.java
+javac -Xlint:unchecked org/jpc/SoundOutTest.java
