8ae0dfd2270252609a4bbe407a4012790149a9c6
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2010-02-13 05:47:09 +0200

    Force UTF-8 in compile scripts
    The file encoding is UTF-8. Using anything else will miscompile at least
    one file, breaking the whole emulator.

--- compile.bat
+++ compile.bat
@@ -1,8 +1,7 @@
-javac -Xlint:unchecked ExceptionDefProcessor.java
+javac -Xlint:unchecked -encoding utf-8 ExceptionDefProcessor.java
 java ExceptionDefProcessor exceptions.def
-javac -Xlint:unchecked JPCApplication.java
-javac -Xlint:unchecked ImageMaker.java
-javac -Xlint:unchecked org/jpc/plugins/*.java
-javac -Xlint:unchecked org/jpc/luaextensions/*.java
-javac -Xlint:unchecked org/jpc/utils/*.java
-javac -Xlint:unchecked SoundTest.java
+javac -Xlint:unchecked -encoding utf-8 JPCApplication.java
+javac -Xlint:unchecked -encoding utf-8 ImageMaker.java
+javac -Xlint:unchecked -encoding utf-8 org/jpc/plugins/*.java
+javac -Xlint:unchecked -encoding utf-8 org/jpc/utils/*.java
+javac -Xlint:unchecked -encoding utf-8 SoundTest.java
--- compile.sh
+++ compile.sh
@@ -3,19 +3,19 @@
 echo "Cleaning up..."
 find -name "*.class" -exec rm {} \;
 echo "Compiling exception list processor..."
-javac -Xlint:unchecked ExceptionDefProcessor.java || exit 1
+javac -Xlint:unchecked -encoding utf-8 ExceptionDefProcessor.java || exit 1
 echo "Transforming exceptions list..."
 java ExceptionDefProcessor exceptions.def || exit 1
 echo "Compiling main application..."
-javac -Xlint:unchecked JPCApplication.java || exit 1
+javac -Xlint:unchecked -encoding utf-8 JPCApplication.java || exit 1
 echo "Compiling ImageMaker..."
-javac -Xlint:unchecked ImageMaker.java || exit 1
+javac -Xlint:unchecked -encoding utf-8 ImageMaker.java || exit 1
 echo "Compiling plugins..."
-javac -Xlint:unchecked org/jpc/plugins/*.java || exit 1
+javac -Xlint:unchecked -encoding utf-8 org/jpc/plugins/*.java || exit 1
 echo "Compiling utilities..."
-javac -Xlint:unchecked org/jpc/utils/*.java || exit 1
+javac -Xlint:unchecked -encoding utf-8 org/jpc/utils/*.java || exit 1
 echo "Compiling SoudTest..."
-javac -Xlint:unchecked SoundTest.java || exit 1
+javac -Xlint:unchecked -encoding utf-8 SoundTest.java || exit 1
 echo "Compling streamtools..."
 cd streamtools || make
 echo "Done."
