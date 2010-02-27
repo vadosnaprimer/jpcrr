2a1b3fc23855385e3a7866c0bc30d019247f1d57
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2010-02-27 12:55:41 +0200

    Also compile modules by default in compile.{sh,bat}

--- compile.bat
+++ compile.bat
@@ -3,6 +3,7 @@ java ExceptionDefProcessor exceptions.def
 javac -Xlint:unchecked -encoding utf-8 JPCApplication.java
 javac -Xlint:unchecked -encoding utf-8 ImageMaker.java
 javac -Xlint:unchecked -encoding utf-8 org/jpc/plugins/*.java
+javac -Xlint:unchecked -encoding utf-8 org/jpc/modules/*.java
 javac -Xlint:unchecked -encoding utf-8 org/jpc/luaextensions/*.java
 javac -Xlint:unchecked -encoding utf-8 org/jpc/utils/*.java
 javac -Xlint:unchecked -encoding utf-8 SoundTest.java
--- compile.sh
+++ compile.sh
@@ -12,6 +12,8 @@ echo "Compiling ImageMaker..."
 javac -Xlint:unchecked -encoding utf-8 ImageMaker.java || exit 1
 echo "Compiling plugins..."
 javac -Xlint:unchecked -encoding utf-8 org/jpc/plugins/*.java || exit 1
+echo "Compiling modules..."
+javac -Xlint:unchecked -encoding utf-8 org/jpc/modules/*.java || exit 1
 echo "Compiling Lua extensions..."
 javac -Xlint:unchecked -encoding utf-8 org/jpc/luaextensions/*.java || exit 1
 echo "Compiling utilities..."
