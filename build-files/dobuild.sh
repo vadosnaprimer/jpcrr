edf370eeecfaf29b66e2e9e88f800c1630fc8bff
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2010-06-04 02:48:08 +0300

    Remove default keyboard layout and read layouts from .jar

--- build-files/dobuild.sh
+++ build-files/dobuild.sh
@@ -11,7 +11,8 @@ cp src/build-files/start-jpcrr.bat . &&
 cp src/build-files/start-jpcrr.sh . &&
 cp src/assemble.jpcrrinit . &&
 mkdir datafiles &&
-cp src/datafiles/* datafiles &&
+cp src/datafiles/extramenu datafiles/extramenu &&
+cp src/datafiles/luakernel.lua datafiles/luakernel.lua &&
 cp src/LICENSE . &&
 mkdir docs &&
 cp src/docs/manual.txt docs &&
@@ -20,7 +21,7 @@ cp --recursive src/lua/* lua &&
 cp --recursive ${3}/* . &&
 cd src &&
 ./compile.sh &&
-jar cvfm jpcrr-${2}.jar build-files/manifest.mod `find -name "*.class"` &&
+jar cvfm jpcrr-${2}.jar build-files/manifest.mod `find -name "*.class"` `find datafiles/keyboards` &&
 cd .. &&
 cp src/jpcrr-${2}.jar . &&
 rm -rf src &&
