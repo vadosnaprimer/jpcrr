605257387d80ff3541d3c98faf53d9574b2f1e2a
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2010-06-10 17:39:05 +0300

    Sightly better subtitle test

--- lua/subtitletest.lua
+++ lua/subtitletest.lua
@@ -12,12 +12,27 @@ for k, v in ipairs(get_long_authors()) do
 	print(v);
 end
 
+headers = jpcrr.movie_headers();
 
 print("");
-info = subtitle_runinfo();
-print(info);
+info1 = subtitle_runinfo();
+info2 = tasvideos_subtitle_this_is_TAS();
+print(info1);
+print(info2);
 
 while true do
         jpcrr.next_frame();
-	render_subtitle_text(info, true, 255, 255, 255);
+	time = jpcrr.clock_time();
+	for k, v in ipairs(headers) do
+		if v[1] == "SUBTITLE1" then
+			if time >= tonumber(v[2]) and time <= tonumber(v[3]) then
+				render_subtitle_text(info1, (v[4] == "TOP"), 255, 128, 0);
+			end
+		end
+		if v[1] == "SUBTITLE2" then
+			if time >= tonumber(v[2]) and time <= tonumber(v[3]) then
+				render_subtitle_text(info2, (v[4] == "TOP"), 255, 128, 0);
+			end
+		end
+	end
 end
