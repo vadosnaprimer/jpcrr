910e3b2d74bd0550ab0df7d3271ccb2c7a4a0aa2
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2010-09-02 02:16:59 +0300

    X264 and oggenc drivers
    Pass input to command-line utilities to encode further in one step...

---
+++ streamtools/output-drv-oggenc.cpp
@@ -0,0 +1,97 @@
+#include "output-drv.hpp"
+#include <cstdio>
+#include <stdexcept>
+#include <string>
+#include <sstream>
+
+namespace
+{
+	std::string expand_options(const std::string& opts)
+	{
+		bool insert = true;
+		if(opts == "")
+			return "";
+		std::string ret = "";
+		for(size_t i = 0; i < opts.length(); i++) {
+			char x[2] = {0, 0};
+			if(insert)
+				ret = ret + " --";
+			insert = false;
+			switch(opts[i]) {
+			case ',':
+				insert = true;
+				break;
+			case '=':
+				ret = ret + " ";
+				break;
+			default:
+				x[0] = opts[i];
+				ret = ret + x;
+			};
+		}
+		ret = ret + " ";
+		return ret;
+	}
+
+	class output_driver_oggenc : public output_driver
+	{
+	public:
+		output_driver_oggenc(const std::string& _filename, const std::string& _options)
+		{
+			filename = _filename;
+			options = _options;
+			set_audio_callback<output_driver_oggenc>(*this, &output_driver_oggenc::audio_callback);
+		}
+
+		~output_driver_oggenc()
+		{
+			pclose(out);
+		}
+
+		void ready()
+		{
+			const audio_settings& a = get_audio_settings();
+
+			std::stringstream commandline;
+			commandline << "oggenc -r -R " << a.get_rate() << " ";
+			commandline << expand_options(options);
+			commandline << "-o " << filename << " -";
+			std::string s = commandline.str();
+			out = popen(s.c_str(), "w");
+			if(!out) {
+				std::stringstream str;
+				str << "Can't run oggenc (" << s << ")";
+				throw std::runtime_error(str.str());
+			}
+		}
+
+		void audio_callback(short left, short right)
+		{
+			uint8_t rawdata[4];
+			rawdata[1] = ((unsigned short)left >> 8) & 0xFF;
+			rawdata[0] = ((unsigned short)left) & 0xFF;
+			rawdata[3] = ((unsigned short)right >> 8) & 0xFF;
+			rawdata[2] = ((unsigned short)right) & 0xFF;
+			if(fwrite(rawdata, 1, 4, out) < 4)
+				throw std::runtime_error("Error writing sample to oggenc");
+		}
+	private:
+		FILE* out;
+		std::string filename;
+		std::string options;
+	};
+
+	class output_driver_oggenc_factory : output_driver_factory
+	{
+	public:
+		output_driver_oggenc_factory()
+			: output_driver_factory("oggenc")
+		{
+		}
+
+		output_driver& make(const std::string& type, const std::string& name, const std::string& parameters)
+		{
+			return *new output_driver_oggenc(name, parameters);
+		}
+	} factory;
+}
---
+++ streamtools/output-drv-x264.cpp
@@ -0,0 +1,119 @@
+#include <cstring>
+#include "output-drv.hpp"
+#include <cstdio>
+#include <stdexcept>
+#include <vector>
+#include <string>
+#include <sstream>
+#include "rgbtorgb.hh"
+
+namespace
+{
+	std::string expand_options(const std::string& opts)
+	{
+		bool insert = true;
+		if(opts == "")
+			return "";
+		std::string ret = "";
+		for(size_t i = 0; i < opts.length(); i++) {
+			char x[2] = {0, 0};
+			if(insert)
+				ret = ret + " --";
+			insert = false;
+			switch(opts[i]) {
+			case ',':
+				insert = true;
+				break;
+			case '=':
+				ret = ret + " ";
+				break;
+			default:
+				x[0] = opts[i];
+				ret = ret + x;
+			};
+		}
+		ret = ret + " ";
+		return ret;
+	}
+
+	class output_driver_x264 : public output_driver
+	{
+	public:
+		output_driver_x264(const std::string& _filename, const std::string& _options)
+		{
+			filename = _filename;
+			options = _options;
+			set_video_callback<output_driver_x264>(*this, &output_driver_x264::video_callback);
+		}
+
+		~output_driver_x264()
+		{
+			pclose(out);
+		}
+
+		void ready()
+		{
+			const video_settings& v = get_video_settings();
+			framesize = 4 * v.get_width() * v.get_height();
+			width = v.get_width();
+
+			std::stringstream commandline;
+			commandline << "x264 ";
+			commandline << expand_options(options);
+			commandline << "- -o " << filename << " " << v.get_width() << "x" << v.get_height();
+			std::string s = commandline.str();
+			out = popen(s.c_str(), "w");
+			if(!out) {
+				std::stringstream str;
+				str << "Can't run x264 (" << s << ")";
+				throw std::runtime_error(str.str());
+			}
+		}
+
+		void video_callback(uint64_t timestamp, const uint8_t* raw_rgbx_data)
+		{
+			std::vector<unsigned char> tmp(framesize * 3 / 8);
+			size_t primarysize = framesize / 4;
+			size_t offs1 = primarysize / 4;
+			size_t offs2 = 0;
+			Convert32To_I420Frame(raw_rgbx_data, &tmp[0], framesize / 4, width);
+			size_t r;
+			if((r = fwrite(&tmp[0], 1, primarysize, out)) < primarysize) {
+				std::stringstream str;
+				str << "Error writing frame to x264 (requested " << primarysize << ", got " << r << ")";
+				throw std::runtime_error(str.str());
+			}
+			//Swap U and V.
+			if((r = fwrite(&tmp[primarysize + offs1], 1, primarysize / 4, out)) < primarysize / 4) {
+				std::stringstream str;
+				str << "Error writing frame to x264 (requested " << primarysize / 4 << ", got " << r << ")";
+				throw std::runtime_error(str.str());
+			}
+			if((r = fwrite(&tmp[primarysize + offs2], 1, primarysize / 4, out)) < primarysize / 4) {
+				std::stringstream str;
+				str << "Error writing frame to x264 (requested " << primarysize / 4 << ", got " << r << ")";
+				throw std::runtime_error(str.str());
+			}
+		}
+	private:
+		FILE* out;
+		std::string filename;
+		std::string options;
+		size_t framesize;
+		size_t width;
+	};
+
+	class output_driver_x264_factory : output_driver_factory
+	{
+	public:
+		output_driver_x264_factory()
+			: output_driver_factory("x264")
+		{
+		}
+
+		output_driver& make(const std::string& type, const std::string& name, const std::string& parameters)
+		{
+			return *new output_driver_x264(name, parameters);
+		}
+	} factory;
+}
