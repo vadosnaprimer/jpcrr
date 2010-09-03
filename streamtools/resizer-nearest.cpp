165e8cbf59faf7cbc1b7c83eaa9b165a05a4b90e
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2010-09-03 17:34:09 +0300

    Add nearest neighbor resizer
    This resizer might be useful as example and for some special encoding
    modes...

---
+++ streamtools/resizer-nearest.cpp
@@ -0,0 +1,46 @@
+#include "resize.hpp"
+#include <stdint.h>
+#include <cmath>
+#include <stdexcept>
+
+namespace
+{
+	inline uint32_t nnscale(uint32_t value, uint32_t trange, uint32_t srange)
+	{
+		//x / trange is as good approximation for value / srange as possible.
+		//=> x is as good approximation for value * trange / srange as possible.
+		return (uint32_t)(((uint64_t)value * trange + srange / 2) / srange);
+	}
+
+	class resizer_local : public resizer
+	{
+	public:
+		void operator()(uint8_t* target, uint32_t twidth, uint32_t theight,
+			const uint8_t* source, uint32_t swidth, uint32_t sheight)
+		{
+			uint32_t* __restrict__ src = (uint32_t*)source;
+			uint32_t* __restrict__ dest = (uint32_t*)target;
+
+			for(uint32_t y = 0; y < theight; y++)
+				for(uint32_t x = 0; x < twidth; x++) {
+					uint32_t _x = nnscale(x, swidth, twidth);
+					uint32_t _y = nnscale(y, sheight, theight);
+					dest[y * twidth + x] = src[_y * swidth + _x];
+				}
+		}
+	};
+
+	class resizer_local_factory : public resizer_factory
+	{
+	public:
+		resizer_local_factory()
+			: resizer_factory("nearest")
+		{
+		}
+
+		resizer& make(const std::string& type)
+		{
+			return *new resizer_local();
+		}
+	} factory;
+}
