906384b167c26938cfc74c86daed9f0509815f8c
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2010-11-02 18:46:41 +0200

    streamtools: Don't let exceptions be thrown out
    Exceptions thrown out might not get printed, only giving strange errors,
    so implement fallback catch handler.

--- streamtools/main.cpp
+++ streamtools/main.cpp
@@ -1,10 +1,38 @@
 #ifndef NO_SDLMAIN
 #include "SDL_main.h"
 #endif
+#include <stdexcept>
+#include <iostream>
 
 int real_main(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
-	return real_main(argc, argv);
+	try {
+		return real_main(argc, argv);
+	} catch(std::exception& e) {
+		std::cerr << "------------------------------------------------------------" << std::endl;
+		if(argv[0])
+			std::cerr << "Program '" << argv[0] << "' terminated due to error:" << std::endl;
+		else
+			std::cerr << "Program <unknown> terminated due to error:" << std::endl;
+		std::cerr << "Cause: " << e.what() << std::endl;
+		std::cerr << "Argument count: " << argc - 1 << std::endl;
+		for(int i = 1; i < argc; i++)
+			std::cerr << "Argument #" << i << ": '" << argv[i] << "'" << std::endl;
+		std::cerr << "------------------------------------------------------------" << std::endl;
+		return 128;
+	} catch(...) {
+		std::cerr << "------------------------------------------------------------" << std::endl;
+		if(argv[0])
+			std::cerr << "Program '" << argv[0] << "' terminated due to error:" << std::endl;
+		else
+			std::cerr << "Program <unknown> terminated due to error:" << std::endl;
+		std::cerr << "Cause: <Unknown>" << std::endl;
+		std::cerr << "Argument count: " << argc - 1 << std::endl;
+		for(int i = 1; i < argc; i++)
+			std::cerr << "Argument #" << i << ": '" << argv[i] << "'" << std::endl;
+		std::cerr << "------------------------------------------------------------" << std::endl;
+		return 128;
+	}
 }
