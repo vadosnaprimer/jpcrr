6ea529125a1856a48a340414d79b24a237f5a803
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2010-12-01 14:36:00 +0200

    Clean up files list and rename some files

--- lua/messagetest.lua
+++
@@ -1,8 +0,0 @@
-while true do
-	message = jpcrr.wait_message();
-	if message then
-		print("Message: " .. message);
-	else
-		print("Interrupted!");
-	end
-end
--- lua/messagetest2.lua
+++
@@ -1,6 +0,0 @@
-while true do
-	message = jpcrr.poll_message();
-	if message then
-		print("Message: " .. message);
-	end
-end
