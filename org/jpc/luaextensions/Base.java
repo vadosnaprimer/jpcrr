5ab77dc3d6aee6995697ed8d1ff3c2fcbdd198be
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2011-06-09 20:10:16 +0300

    Add jpcrr.state for getting various status variables
    Add jpcrr.state for getting various status variables and deprecate the
    old status functions.

--- org/jpc/luaextensions/Base.java
+++ org/jpc/luaextensions/Base.java
@@ -151,29 +151,6 @@ public class Base extends LuaPlugin.LuaResource
         return 0;
     }
 
-    public static int luaCB_vga_resolution(Lua l, LuaPlugin plugin)
-    {
-        l.push(new Double(plugin.getXResolution()));
-        l.push(new Double(plugin.getYResolution()));
-        return 2;
-    }
-
-    public static int luaCB_pc_running(Lua l, LuaPlugin plugin)
-    {
-        l.pushBoolean(plugin.getPCRunning());
-        return 1;
-    }
-
-    public static int luaCB_clock_time(Lua l, LuaPlugin plugin)
-    {
-        Clock clk = (Clock)plugin.getComponent(Clock.class);
-        if(clk != null)
-            l.push(new Double(clk.getTime()));
-        else
-            l.pushNil();
-        return 1;
-    }
-
     public static int luaCB_wait_event(Lua l, LuaPlugin plugin)
     {
         LuaPlugin.Event msg = plugin.waitEvent();
@@ -210,12 +187,6 @@ public class Base extends LuaPlugin.LuaResource
         return 1;
     }
 
-    public static int luaCB_pc_connected(Lua l, LuaPlugin plugin)
-    {
-        l.pushBoolean(plugin.getPCConnected());
-        return 1;
-    }
-
     public static int luaCB_stringlessthan(Lua l, LuaPlugin plugin)
     {
         int ret = 0;
@@ -244,112 +215,5 @@ public class Base extends LuaPlugin.LuaResource
 
         l.pushBoolean(ret == 1);
         return 1;
-    }
-
-    public static int luaCB_frame_number(Lua l, LuaPlugin plugin)
-    {
-        DisplayController dc = ((DisplayController)plugin.getComponent(DisplayController.class));
-        if(dc == null)
-            l.pushNil();
-        else
-            l.pushNumber((double)dc.getFrameNumber());
-        return 1;
-    }
-
-    public static int luaCB_movie_length(Lua l, LuaPlugin plugin)
-    {
-        PC.ResetButton brb = ((PC.ResetButton)plugin.getComponent(PC.ResetButton.class));
-        if(brb == null) {
-            l.pushNil();
-            return 1;
-        }
-        EventRecorder rec = brb.getRecorder();
-        long lastTime = rec.getLastEventTime();
-        long attachTime = rec.getAttachTime();
-        long time = 0;
-        if(attachTime < lastTime)
-            time = lastTime - attachTime;
-        l.push(new Double(time));
-        return 1;
-    }
-
-    public static int luaCB_movie_rerecords(Lua l, LuaPlugin plugin)
-    {
-        PC.ResetButton brb = ((PC.ResetButton)plugin.getComponent(PC.ResetButton.class));
-        if(brb == null) {
-            l.pushNil();
-            return 1;
-        }
-        EventRecorder rec = brb.getRecorder();
-        l.push(rec.getRerecordCount().doubleValue());
-        return 1;
-    }
-
-    public static int luaCB_movie_headers(Lua l, LuaPlugin plugin)
-    {
-        PC.ResetButton brb = ((PC.ResetButton)plugin.getComponent(PC.ResetButton.class));
-        if(brb == null) {
-            l.pushNil();
-            return 1;
-        }
-        EventRecorder rec = brb.getRecorder();
-        String[][] headers = rec.getHeaders();
-        LuaTable ret = l.newTable();
-        if(headers != null)
-            for(int i = 0; i < headers.length; i++) {
-                LuaTable tab = l.newTable();
-                int j = 1;
-                if(headers[i] != null)
-                    for(String p : headers[i])
-                        l.setTable(tab, new Double(j++), p);
-                l.setTable(ret, new Double(i + 1), tab);
-            }
-        l.push(ret);
-        return 1;
-    }
-
-    public static int luaCB_vga_edge_count(Lua l, LuaPlugin plugin)
-    {
-        VGACard card = (VGACard)plugin.getComponent(VGACard.class);
-        if(card != null)
-            l.push(new Double(card.getVretraceEdgeCount()));
-        else
-            l.pushNil();
-        return 1;
-    }
-
-    public static int luaCB_vga_scroll_count(Lua l, LuaPlugin plugin)
-    {
-        VGACard card = (VGACard)plugin.getComponent(VGACard.class);
-        if(card != null)
-            l.push(new Double(card.getScrollCount()));
-        else
-            l.pushNil();
-        return 1;
-    }
-
-    public static int luaCB_project_id(Lua l, LuaPlugin plugin)
-    {
-        PC.ResetButton brb = ((PC.ResetButton)plugin.getComponent(PC.ResetButton.class));
-        if(brb == null) {
-            l.pushNil();
-            return 1;
-        }
-        EventRecorder rec = brb.getRecorder();
-        l.push(rec.getProjectID());
-        return 1;
-    }
-
-    public static int luaCB_get_irq0_info(Lua l, LuaPlugin plugin)
-    {
-        IntervalTimer clk = (IntervalTimer)plugin.getComponent(IntervalTimer.class);
-        if(clk != null) {
-            l.push(new Double(clk.getIRQ0Cycles()));
-            l.push(new Double(clk.getIRQ0Rate()));
-        } else {
-            l.pushNil();
-            l.pushNil();
-        }
-        return 2;
     }
 }
---
+++ org/jpc/luaextensions/Status.java
@@ -0,0 +1,137 @@
+/*
+    JPC-RR: A x86 PC Hardware Emulator
+    Release 1
+
+    Copyright (C) 2007-2009 Isis Innovation Limited
+    Copyright (C) 2009-2011 H. Ilari Liusvaara
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2 as published by
+    the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    Based on JPC x86 PC Hardware emulator,
+    A project from the Physics Dept, The University of Oxford
+
+    Details about original JPC can be found at:
+
+    www-jpc.physics.ox.ac.uk
+
+*/
+
+package org.jpc.luaextensions;
+
+import mnj.lua.*;
+
+import org.jpc.emulator.Clock;
+import org.jpc.emulator.PC;
+import org.jpc.emulator.pci.peripheral.VGACard;
+import org.jpc.emulator.DisplayController;
+import org.jpc.emulator.EventRecorder;
+import org.jpc.emulator.motherboard.IntervalTimer;
+import org.jpc.plugins.LuaPlugin;
+
+//Locking this class is used for preventing termination and when terminating.
+public class Status extends LuaPlugin.LuaResource
+{
+    public void destroy()
+    {
+    }
+
+    private Status(LuaPlugin plugin)
+    {
+        super(plugin);
+    }
+
+    public static int luaCB_status(Lua l, LuaPlugin plugin)
+    {
+        if(!plugin.getPCConnected())
+            return 0;
+        LuaTable tab = l.newTable();
+        l.setTable(tab, "running", plugin.getPCRunning());
+        l.push(tab);
+        Clock clk = (Clock)plugin.getComponent(Clock.class);
+        if(clk != null)
+            l.setTable(tab, "time", new Double(clk.getTime()));
+        l.setTable("resolution_x", plugin.getXResolution());
+        l.setTable("resolution_y", plugin.getXResolution());
+        DisplayController dc = ((DisplayController)plugin.getComponent(DisplayController.class));
+        if(dc != null)
+            l.setTable(tab, "frame", new Double(dc.getFrameNumber()));
+        PC.ResetButton brb = ((PC.ResetButton)plugin.getComponent(PC.ResetButton.class));
+        if(brb != null) {
+            EventRecorder rec = brb.getRecorder();
+            long lastTime = rec.getLastEventTime();
+            long attachTime = rec.getAttachTime();
+            long time = 0;
+            if(attachTime < lastTime)
+                time = lastTime - attachTime;
+            l.setTable(tab, "movie_length", new Double(time));
+            l.setTable(tab, "movie_rerecords", rec.getRerecordCount().doubleValue());
+            String[][] headers = rec.getHeaders();
+            LuaTable ret = l.newTable();
+            if(headers != null)
+                for(int i = 0; i < headers.length; i++) {
+                    LuaTable tab = l.newTable();
+                    int j = 1;
+                    if(headers[i] != null)
+                        for(String p : headers[i])
+                            l.setTable(tab, new Double(j++), p);
+                    l.setTable(ret, new Double(i + 1), tab);
+                }
+            l.setTable(tab, "movie_headers", ret);
+            l.setTable(tab, "project_id", rec.getProjectID());
+        }
+        VGACard card = (VGACard)plugin.getComponent(VGACard.class);
+        if(card != null) {
+            l.setTable(tab, "vga_scrolls", new Double(card.getScrollCount()));
+            l.setTable(tab, "vga_retraces_seen", new Double(card.getVretraceEdgeCount()));
+        }
+        IntervalTimer clk = (IntervalTimer)plugin.getComponent(IntervalTimer.class);
+        if(clk != null) {
+            l.setTable(tab, "irq0_dispatched", new Double(clk.getIRQ0Cycles()));
+            l.setTable(tab, "irq0_frequency", new Double(clk.getIRQ0Rate()));
+        }
+        Keyboard key = (Keyboard)plugin.getComponent(Keyboard.class);
+        if(key != null) {
+            int status = key.getLEDStatus();
+            if(status < 0) {
+                l.setTable(tab, "keyboard_leds_valid", false);
+            } else {
+                l.setTable(tab, "keyboard_leds_valid", true);
+                l.setTable(tab, "keyboard_num_lock", (status & 2) != 0);
+                l.setTable(tab, "keyboard_caps_lock", (status & 4) != 0);
+                l.setTable(tab, "keyboard_scroll_lock", (status & 1) != 0);
+            }
+            l.setTable(tab, "mouse_dx_pending", new Double(kbd.getMouseXPendingMotion()));
+            l.setTable(tab, "mouse_dy_pending", new Double(kbd.getMouseYPendingMotion()));
+            l.setTable(tab, "mouse_dz_pending", new Double(kbd.getMouseZPendingMotion()));
+            int b = kbd.getMouseButtonStatus();
+            l.setTable(tab, "mouse_button_1", (b & 1) != 0);
+            l.setTable(tab, "mouse_button_2", (b & 2) != 0);
+            l.setTable(tab, "mouse_button_3", (b & 4) != 0);
+            l.setTable(tab, "mouse_button_4", (b & 8) != 0);
+            l.setTable(tab, "mouse_button_5", (b & 16) != 0);
+        }
+        Joystick joy = (Joystick)plugin.getComponent(Joystick.class);
+        if(joy != null) {
+            l.setTable(tab, "joystick_ax", new Double(joy.axisHoldTime(0, false)));
+            l.setTable(tab, "joystick_ay", new Double(joy.axisHoldTime(1, false)));
+            l.setTable(tab, "joystick_bx", new Double(joy.axisHoldTime(2, false)));
+            l.setTable(tab, "joystick_by", new Double(joy.axisHoldTime(3, false)));
+            l.setTable(tab, "joystick_aa", joy.buttonState(0, false));
+            l.setTable(tab, "joystick_ab", joy.buttonState(1, false));
+            l.setTable(tab, "joystick_ba", joy.buttonState(2, false));
+            l.setTable(tab, "joystick_bb", joy.buttonState(3, false));
+        }
+        return 1;
+    }
+}
